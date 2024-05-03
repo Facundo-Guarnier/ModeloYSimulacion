406f24b76fb0818ab95c391e130c242dc515985c
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-05-03 12:28:53 -0300

    Correccion de tp3, 4 y 5.

--- app.py
+++ app.py
@@ -1,21 +1,218 @@
+import random
+from matplotlib import pyplot as plt
 from clases.Calentador import Calentador
 from clases.Liquido import Liquido
 from clases.Material import Material
 from clases.Recipiente import Recipiente
 
 
+class App:
+    def __init__(self) -> None:
+        """
+        Clase que representa la aplicación principal.
+        """
+        pass
+    
+    def setLiquido(self, liquido:Liquido) -> None:
+        self.liquido = liquido
+
+    def setMaterial(self, materia:Material) -> None:
+        self.material = materia
+
+    def setRecipiente(self, recipiente:Recipiente) -> None:
+        self.recipiente = recipiente
+    
+    def setCalentador(self, calentador:Calentador) -> None:
+        self.calentador = calentador
+    
+    def tp1_a(self) -> None:
+        """
+        Calcular la potencia, corriente y resistencia del calentador.
+        """
+        print(f"Tp1 A:\nPotencia: {self.calentador.potencia:.2f} W\nCorriente: {self.calentador.potencia/self.calentador.tension:.2f} A\nResistencia: {self.calentador.resistencia:.2f} Ω\nCalor necesario: {self.calentador.cantidad_calor_necesaria():.2f} J")
+    
+    
+    def tp1_b(self) -> None:
+        """
+        Calcular el aumento de temperatura luego de 1s de conectar la alimentación, suponiendo que no existe pérdida de calor.
+        """
+        
+        cantidad_calor = self.calentador.potencia * 1
+        
+        print(
+            f"Tp1 B:\nAumento de temperatura luego de 1s: {self.calentador.cambio_temperatura_por_segundo(cantidad_calor):.2f} °C"
+        )
+    
+    
+    def tp2(self) -> None:
+        """
+        Graficar el aumento de temperatura del agua en el recipiente sin perdida de calor (lineal).
+        """
+        
+        plt.plot(range(self.calentador.tiempo_objetivo), self.calentador.aumento_temperatura_sin_perdida())
+        plt.xlabel("Tiempo (s)")
+        plt.ylabel("Temperatura (°C)")
+        plt.title("TP2: Aumento de temperatura del agua en el recipiente \nsin perdida de calor")
+        plt.grid()
+        plt.show()
+    
+    
+    def tp3(self) -> None:
+        """
+        Calcular la pérdida de calor de nuestro dispositivo, según las especificaciones de diseño. 
+        """
+        
+        print(f"Tp3:\nPerdida de calor: {self.calentador.cantidad_calor_perdido_total():.2f} J")
+    
+    
+    def tp4(self) -> None:
+        """
+        Graficar la temperatura del fluido dentro del calentador sin pérdidas y con pérdidas para cada tick de tiempo, 
+        hasta llegar al tiempo deseado para que el dispositivo cumpla su tarea.
+
+        Para realizar el gráfico con pérdidas, se debe considerar los vatios efectivos entregados al fluido restando al 
+        calor producido por la resistencia, el calor perdido por las paredes del recipiente. Con este calor efectivo se
+        calcula la variación de temperatura del fluido para cada tick de tiempo.
+        """
+        
+        max_temp = max(self.calentador.aumento_temperatura_con_perdida())
+        
+        plt.plot(range(self.calentador.tiempo_objetivo), self.calentador.aumento_temperatura_sin_perdida(), label="Sin perdida", linestyle="-")
+        plt.plot(range(self.calentador.tiempo_objetivo), self.calentador.aumento_temperatura_con_perdida(), label="Con perdida" , linestyle="-")
+        plt.axhline(y=max_temp, color='g', linestyle='--', label="Temperatura final") 
+        plt.text(0.5, max_temp, f'{max_temp:.2f}', color='g', fontsize=10, ha='center')
+        plt.xlabel("Tiempo (s)")
+        plt.ylabel("Temperatura (°C)")
+        plt.title("TP4: Aumento de temperatura del agua en el\nrecipiente con/sin perdida de calor")
+        plt.grid()
+        plt.legend()
+        plt.show()
+    
+    
+    def tp5_a(self) -> None:
+        """
+        Generar familias de curvas con distribuciones normales y uniformes con:
+        - Distribución uniforme de 5 valores próximos de resistencias
+        """
+        resistencia_original = self.calentador.resistencia
+        resistencias = [ resistencia_original * (1 + random.uniform(-0.1, 0.1)) for _ in range(5)]
+        
+        for resistencia in resistencias:
+            self.calentador.resistencia = resistencia
+            self.calentador.potencia = self.calentador.tension**2 / resistencia
+            plt.plot(range(self.calentador.tiempo_objetivo), self.calentador.aumento_temperatura_con_perdida(), label=f"{resistencia:.2f} Ω" , linestyle="-")
+        
+        self.calentador.resistencia = resistencia_original
+        
+        plt.xlabel("Tiempo (s)")
+        plt.ylabel("Temperatura (°C)")
+        plt.title("TP5 A: Variacion de resistencia en el calentador")
+        plt.grid()
+        plt.legend()
+        plt.show()
+    
+    
+    def tp5_b(self) -> None:
+        """
+        Generar familias de curvas con distribuciones normales y uniformes con:
+        - Distribución normal de 5 temperaturas iniciales del agua. Media 10, desvío standard=5
+        """
+        temperatura_inicial_original = self.calentador.temperatura_liquido_inicial
+        temperaturas_iniciales = [random.normalvariate(10, 5) for _ in range(5)]
+        
+        for temperatura_inicial in temperaturas_iniciales:
+            self.calentador.temperatura_liquido_inicial = temperatura_inicial
+            plt.plot(range(self.calentador.tiempo_objetivo), self.calentador.aumento_temperatura_con_perdida(), label=f"{temperatura_inicial:.2f} °C" , linestyle="-")
+            
+        self.calentador.temperatura_liquido_inicial = temperatura_inicial_original
+        
+        plt.xlabel("Tiempo (s)")
+        plt.ylabel("Temperatura (°C)")
+        plt.title("TP5 B: Variacion de temperatura inicial del agua")
+        plt.grid()
+        plt.legend()
+        plt.show()
+    
+    
+    def tp5_c(self) -> None:
+        """
+        Generar familias de curvas con distribuciones normales y uniformes con:
+        - Distribución uniforme de 8 temperaturas iniciales del ambiente, entre -20 y 50 grados.
+        """
+        temperaturas_ambiente_original = self.calentador.temperatura_ambiente
+        temperaturas_ambiente = [random.uniform(-20, 50) for _ in range(8)]
+        
+        for temperatura_ambiente in temperaturas_ambiente:
+            self.calentador.temperatura_ambiente = temperatura_ambiente
+            plt.plot(range(self.calentador.tiempo_objetivo), self.calentador.aumento_temperatura_con_perdida(), label=f"{temperatura_ambiente:.2f} °C" , linestyle="-")
+        
+        self.calentador.temperatura_ambiente = temperaturas_ambiente_original
+        
+        plt.xlabel("Tiempo (s)")
+        plt.ylabel("Temperatura (°C)")
+        plt.title("TP5 C: Variacion de temperatura ambiente")
+        plt.grid()
+        plt.legend()
+        plt.show()
+    
+    
+    def tp5_d(self) -> None:
+        """
+        Generar familias de curvas con distribuciones normales y uniformes con:
+        - Distribución normal de 5 valores de tension de alimentación Media 12 SD:4 o Media 220, SD 40.
+        """
+        tension_original = self.calentador.tension
+        tensiones = [random.normalvariate(220, 40) for _ in range(5)] 
+
+        for tension in tensiones:
+            self.calentador.tension = tension
+            self.calentador.potencia = tension**2 / self.calentador.resistencia
+            plt.plot(range(self.calentador.tiempo_objetivo), self.calentador.aumento_temperatura_con_perdida(), label=f"{tension:.2f} V" , linestyle="-")
+        
+        self.calentador.tension = tension_original
+        
+        plt.xlabel("Tiempo (s)")
+        plt.ylabel("Temperatura (°C)")
+        plt.title("TP5 D: Variacion de tension de alimentación")
+        plt.grid()
+        plt.legend()
+        plt.show()
+    
+    
+    def tp5_e(self) -> None:
+        """
+        Generar familias de curvas con distribuciones normales y uniformes con:
+        - Simulaciones que contengan todas las familias de curvas previas.
+        """
+        raise NotImplementedError("Tp5 E")
+
+    def tp6(self) -> None:
+        """
+        """
+        raise NotImplementedError("Tp6")
+
+
 if __name__ == '__main__':
+    a = App()
     
     agua = Liquido(
         nombre="Agua",
         densidad=1, 
         calor_especifico=4.186,
     )
+    print(agua)
+    a.setLiquido(agua)
+    
+    print("-"*10)
     
     telgopor = Material(
         nombre="Telgopor",
         conductividad_térmica=0.035,
     )
+    print(telgopor)
+    a.setMaterial(telgopor)
+    
+    print("-"*10)
     
     cilindro = Recipiente(
         altura=6.36619, 
@@ -24,6 +221,10 @@ if __name__ == '__main__':
         espesor_aislante=0.01, 
         liquido=agua
     )
+    print(cilindro)
+    a.setRecipiente(cilindro)
+    
+    print("-"*10)
     
     calentador = Calentador(
         temperatura_liquido_inicial=30,
@@ -33,23 +234,26 @@ if __name__ == '__main__':
         recipiente=cilindro,
         tension=220,
     )
+    calentador.tension
+    a.setCalentador(calentador)
+    print(calentador)
+    
+    print("+"*10)
+    print("+"*10)
     
     
-    print(agua)
+    a.tp1_a()
     print("-"*10)
-    print(telgopor)
+    a.tp1_b()
     print("-"*10)
-    print(cilindro)
+    a.tp2()
     print("-"*10)
+    a.tp3()
+    a.tp4()
+    a.tp5_a()
+    a.tp5_b()
+    a.tp5_c()
+    a.tp5_d()
     
-    
-    print(calentador.tp1_a())
-    print("-"*10)
-    print(calentador.tp1_b())
-    print("-"*10)
-    print(calentador.tp2())
-    print("-"*10)
-    print(calentador.tp3())
-    print("-"*10)
-    print(calentador.tp4())
-    print("-"*10)
+    a.tp5_e()
+    a.tp6()
--- clases/Calentador.py
+++ clases/Calentador.py
@@ -1,145 +1,106 @@
+import random
 from clases.Recipiente import Recipiente
 import matplotlib.pyplot as plt
 
 
 class Calentador:
-    """
-    Clase que representa un calentador de agua teniendo en cuenta todos los factores que intervienen en el proceso.
-    - Temperatura ambiente
-    - Temperatura inicial
-    - Temperatura final
-
-    - Tiempo objetivo
-
-    - Tipo de recipiente
-
-    - Tension
-    - Resistencia
-    - Potencia
-    """
-    
     def __init__(self, recipiente:Recipiente, tension:float, temperatura_liquido_inicial:float, temperatura_liquido_final:float, temperatura_ambiente:float, tiempo_objetivo:int) -> None:
+        """
+        Clase que representa un calentador de agua teniendo en cuenta todos los factores que intervienen en el proceso.
+        
+        Args:
+            recipiente (Recipiente): Recipiente que contiene el liquido a calentar.
+            tension (float): Voltaje de la fuente de alimentación (V).
+            temperatura_liquido_inicial (float): Temperatura inicial del liquido en el recipiente.
+            temperatura_liquido_final (float): Temperatura final deseada del liquido en el recipiente.
+            temperatura_ambiente (float): Temperatura ambiente.
+            tiempo_objetivo (int): Tiempo en segundos que se desea alcanzar la temperatura final.
+        """
         self.temperatura_liquido_inicial = temperatura_liquido_inicial
         self.temperatura_liquido_final = temperatura_liquido_final
         self.temperatura_ambiente = temperatura_ambiente
         
         self.tiempo_objetivo = tiempo_objetivo
-        
         self.recipiente = recipiente
-        
         self.tension = tension
-        self.resistencia = 0.0
-        self.potencia = 0.0
-    
-    def tp1_a(self) -> str:     
-        """
-        Calcular la potencia, corriente y resistencia del calentador.
-        """
-        cantidad_calor = self.recipiente.masa_liquido * self.recipiente.liquido.calor_especifico * (self.temperatura_liquido_final - self.temperatura_liquido_inicial)
         
-        self.potencia = cantidad_calor / self.tiempo_objetivo
+        self.potencia = self.cantidad_calor_necesaria() / self.tiempo_objetivo
         self.resistencia = self.tension / (self.potencia / self.tension)
-        
-        return (
-            f"Tp1: A\n" +
-            f"Cantidad de calor (Q): {cantidad_calor:.2f} J\n" +
-            f"Potencia: {self.potencia:.2f} W\n" +
-            f"Corriente: {self.potencia/self.tension:.2f} A\n" +
-            f"Resistencia: {self.resistencia:.2f} Ω"
-        )
-
-    def tp1_b(self) -> str:
-        """
-        Calcular el aumento de temperatura luego de 1s de conectar la alimentación, suponiendo que no existe pérdida de calor.
-        """
-        cantidad_calor = self.potencia*1
-        cambio_temperatura = cantidad_calor / (self.recipiente.masa_liquido * self.recipiente.liquido.calor_especifico)
-        
-        return (
-            f"Tp1: B\n" +
-            f"Aumento de temperatura luego de 1s: {cambio_temperatura:.2f} °C"
-        )
     
-    def tp2(self) -> str:
+    
+    def cantidad_calor_necesaria(self) -> float:     
         """
-        Graficar el aumento de temperatura del agua en el recipiente sin perdida de calor (lineal).
+        Calcular la cantidad de calor necesaria para elevar la temperatura del agua en el recipiente, sin considerar la perdía de calor.
+        
+        Returns:
+            float: Cantidad de calor necesaria para elevar la temperatura del agua en el recipiente: Q = m * c * ΔT -> J
+        """
+        return float(self.recipiente.masa_liquido * self.recipiente.liquido.calor_especifico * (self.temperatura_liquido_final - self.temperatura_liquido_inicial))
+    
+    
+    def cambio_temperatura_por_segundo(self, cantidad_calor:float) -> float:
+        """
+        Calcular el cambio de temperatura del agua en el recipiente, dados la cantidad de calor y el tiempo transcurrido, sin considerar la perdía de calor.
+        
+        Args:
+            cantidad_calor (float): Cantidad de calor entregada al agua en el recipiente.
+        
+        Returns:
+            float: Cambio de temperatura del agua en el recipiente: ΔT = Q / (m * c) -> C°
+        """
+        return cantidad_calor / (self.recipiente.masa_liquido * self.recipiente.liquido.calor_especifico)
+    
+    
+    def aumento_temperatura_sin_perdida (self) -> list[float]:
+        """
+        Calcular el aumento de temperatura del agua en el recipiente sin perdida de
+        calor durante todo el periodo de tiempo.
+        
+        Returns:
+            list[float]: Lista con el aumento de temperatura del agua en el recipiente en cada segundo.
         """
         temperaturas = []
-        temperatura_agua = self.temperatura_liquido_inicial
         for segundo in range(self.tiempo_objetivo):
-            cantidad_calor = self.potencia
-            masa_agua = self.recipiente.masa_liquido
-            calor_especifico_agua = self.recipiente.liquido.calor_especifico
-            cambio_temperatura = cantidad_calor / (masa_agua * calor_especifico_agua)
-            
-            temperatura_agua += cambio_temperatura
-            temperaturas.append(temperatura_agua)
+            cambio_temperatura_sin_perdida = (self.potencia * segundo) / (self.recipiente.masa_liquido * self.recipiente.liquido.calor_especifico)
+            temperatura_agua_sin_perdida = self.temperatura_liquido_inicial + cambio_temperatura_sin_perdida
+            temperaturas.append(temperatura_agua_sin_perdida)
         
-        
-        plt.plot(range(self.tiempo_objetivo), temperaturas)
-        plt.xlabel("Tiempo (s)")
-        plt.ylabel("Temperatura (°C)")
-        plt.title("Aumento de temperatura del agua en el recipiente \nsin perdida de calor")
-        plt.grid()
-        plt.show()
-        return (
-            "Tp2 \n" +
-            "Gráfica realizada con éxito."
-        )
+        return temperaturas
     
-    def tp3(self) -> str:
-        """
-        Calcular la pérdida de calor de nuestro dispositivo, según las especificaciones de diseño. 
-        Calor perdido en Watts/grado Kelvin = Coeficiente de Conductividad Térmica W/m grado Kelvin * Sup/Esp/m
-        """
-        
-        cantidad_calor_perdido =  self.recipiente.material.conductividad_térmica * (self.recipiente.superficie/self.recipiente.espesor_aislante)/self.recipiente.masa_liquido
-
-        return (
-            f"Tp3\n" +
-            f"Cantidad de calor perdido: {cantidad_calor_perdido:.2f} W"
-        )
     
-    def tp4(self) -> str:
+    def cantidad_calor_perdido_total(self) -> float:
         """
-        Graficar la temperatura del fluido dentro del calentador sin pérdidas y con pérdidas para cada tick de tiempo, 
-        hasta llegar al tiempo deseado para que el dispositivo cumpla su tarea.
-
-        Para realizar el gráfico con pérdidas, se debe considerar los vatios efectivos entregados al fluido restando al 
-        calor producido por la resistencia, el calor perdido por las paredes del recipiente. Con este calor efectivo se
-        calcula la variación de temperatura del fluido para cada tick de tiempo.
-        """
+        Calcular la cantidad de calor perdido total por el recipiente.
         
-        temperaturas_sin_perdida = []
+        Returns:
+            float: Cantidad de calor perdido total por el recipiente: Q = k * A * ΔT / d -> J
+        """
+        return self.recipiente.material.conductividad_térmica * (self.recipiente.superficie / self.recipiente.espesor_aislante) * (self.temperatura_liquido_final - self.temperatura_ambiente) / self.recipiente.masa_liquido
+    
+    
+    def aumento_temperatura_con_perdida(self) -> list[float]:
+        """
+        Calcular el aumento de temperatura del agua en el recipiente con perdida de calor durante todo el periodo de tiempo.
+        
+        Returns:
+            list[float]: Lista con el aumento de temperatura del agua en el recipiente en cada segundo.
+        """
+        temperaturas_sin_perdida = self.aumento_temperatura_sin_perdida()
         temperaturas_con_perdida = []
         
         for segundo in range(self.tiempo_objetivo):
-        
-            #! Sin perdida
-            cambio_temperatura_sin_perdida = (self.potencia * segundo) / (self.recipiente.masa_liquido * self.recipiente.liquido.calor_especifico)
-            temperatura_agua_sin_perdida = self.temperatura_liquido_inicial + cambio_temperatura_sin_perdida
-            temperaturas_sin_perdida.append(temperatura_agua_sin_perdida)
-        
-            #! Con perdida
-            calor_perdido =  self.recipiente.material.conductividad_térmica * (self.recipiente.superficie/self.recipiente.espesor_aislante)/self.recipiente.masa_liquido
-        
-            calor_perdido_total = calor_perdido * (temperatura_agua_sin_perdida - self.temperatura_ambiente) * segundo
-            cantidad_calor_efectivo = (self.potencia * segundo) - calor_perdido_total
+            # calor_perdido_por_segundo = self.recipiente.material.conductividad_térmica * (self.recipiente.superficie / self.recipiente.espesor_aislante) * (temperatura_agua_sin_perdida - self.temperatura_ambiente) / self.recipiente.masa_liquido
+            # calor_total_perdido = calor_perdido_por_segundo * segundo
+            # calor_total_entregado = self.potencia * segundo
+            # calor_efectivo = calor_total_entregado - calor_total_perdido 
+            # delta_T_efectivo = calor_efectivo / (self.recipiente.masa_liquido * self.recipiente.liquido.calor_especifico)
+            # temperaturas_con_perdida.append(self.temperatura_liquido_inicial + delta_T_efectivo)
             
-            delta_T_efectivo = cantidad_calor_efectivo / (self.recipiente.masa_liquido * self.recipiente.liquido.calor_especifico)
+            calor_perdido_por_segundo = self.recipiente.material.conductividad_térmica * (self.recipiente.superficie / self.recipiente.espesor_aislante) * (temperaturas_sin_perdida[segundo] - self.temperatura_ambiente) / self.recipiente.masa_liquido
+            calor_total_perdido = calor_perdido_por_segundo * segundo
+            calor_total_entregado = self.potencia * segundo
+            calor_efectivo = calor_total_entregado - calor_total_perdido 
+            delta_T_efectivo = calor_efectivo / (self.recipiente.masa_liquido * self.recipiente.liquido.calor_especifico)
             temperaturas_con_perdida.append(self.temperatura_liquido_inicial + delta_T_efectivo)
-
         
-        plt.plot(range(self.tiempo_objetivo), temperaturas_sin_perdida, label="Sin perdida", linestyle="-")
-        plt.plot(range(self.tiempo_objetivo), temperaturas_con_perdida, label="Con perdida" , linestyle="-")
-        plt.xlabel("Tiempo (s)")
-        plt.ylabel("Temperatura (°C)")
-        plt.title("Aumento de temperatura del agua en el\nrecipiente con/sin perdida de calor.")
-        plt.grid()
-        plt.legend()
-        plt.show()
-        return (
-            "Tp4 \n" +
-            "Gráfica realizada con éxito.\n" + 
-            f"Tempera maxima con pedida de calor: {temperaturas_con_perdida[-1]:.2f}°C\n"
-        )
+        return temperaturas_con_perdida
